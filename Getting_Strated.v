c2bebaa45f273e167495e0661299a59d1778013b
YZchen3467 <r07945002@g.ntu.edu.tw>
2022-05-31 17:49:35 +0800

    HDLbits Getting Started upload

---
+++ Getting_Strated.v
@@ -0,0 +1,16 @@
+/*Getting Started*/
+module top_module( output one );
+
+// Insert your code here
+    assign one = 1;
+
+endmodule
+
+///////////////////////////////////
+
+/*Output Zero*/
+module top_module(output zero)
+
+	assign zero = 0;
+
+endmodule

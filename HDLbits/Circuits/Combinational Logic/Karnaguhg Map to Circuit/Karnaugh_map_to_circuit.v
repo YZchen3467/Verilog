4c06ff21a02292e2d9456dea6b07c9975a2b1dae
YZchen3467 <94513990+YZchen3467@users.noreply.github.com>
2022-06-13 08:26:05 +0800

    Update Karnaugh_map_to_circuit.v
    add second way for K-map implemented

--- HDLbits/Circuits/Combinational Logic/Karnaguhg Map to Circuit/Karnaugh_map_to_circuit.v
+++ HDLbits/Circuits/Combinational Logic/Karnaguhg Map to Circuit/Karnaugh_map_to_circuit.v
@@ -87,6 +87,15 @@ module top_module (
     input c,
     input d,
     output [3:0] mux_in
+);
+    module top_module (
+    input c,
+    input d,
+    output [3:0] mux_in
 ); 
-    assign mux_in = {c&d, ~d, 1'b0, d|c};
+	assign mux_in[3] = c&d;
+	assign mux_in[2] = ~d;
+	assign mux_in[1] = 1'b0;
+	assign mux_in[0] = d|c;
+    /*assign mux_in = {c&d, ~d, 1'b0, d|c};*/
 endmodule

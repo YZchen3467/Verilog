5e3a4cd1689fdfb3ad450c191c046f9df1cbca4d
unknown <r07945002@g.ntu.edu.tw>
2022-06-20 22:46:00 +0800

    updated Counter circuit

--- HDLbits/Circuits/Sequential Logic/Counters/Counters.v
+++ HDLbits/Circuits/Sequential Logic/Counters/Counters.v
@@ -114,7 +114,7 @@ endmodule
 
 ////////////////////////////////////
 
-//4-digit decimal counter (not understand yet)
+//4-digit decimal counter
 module top_module (
     input clk,
     input reset,   // Synchronous active-high reset
@@ -147,4 +147,78 @@ module top_module (
     assign ena[1] = q[3:0] == 4'd9;
     assign ena[2] = q[7:4] == 4'd9 && q[3:0] == 4'd9; 
     assign ena[3] = q[11:8] == 4'd9 && q[7:4] == 4'd9 && q[3:0] == 4'd9; 
+endmodule
+
+////////////////////////////////////
+
+//12-hour clock
+module top_module(
+    input clk,
+    input reset,
+    input ena,
+    output pm,
+    output [7:0] hh,
+    output [7:0] mm,
+    output [7:0] ss);
+
+    reg p;	//0 was am, 1 was pm
+    reg [7:0] h;
+    reg [7:0] m;
+    reg [7:0] s;
+    
+	always@(posedge clk) begin
+		if(reset) begin
+			h <= 8'h12;
+			m <= 8'h0;
+			s <= 8'h0;
+			p <= 1'b0;
+		end	
+		else begin
+			if(ena) begin
+				if(s < 8'h59) begin
+					if(s[3:0] < 4'h9) begin
+						s[3:0] <= s[3:0] + 1'h1;
+					end
+					else begin
+					s[3:0] <= 4'h0;
+					s[7:4] <= s[7:4] + 1'h1;
+					end
+				end
+				else begin
+					s <= 8'h0;
+					if(m < 8'h59) begin
+						if(m[3:0] < 4'h9) begin
+							m[3:0] <= m[3:0] + 1'h1;
+						end
+						else begin
+							m[3:0] <= 4'h0;
+							m[7:4] <= m[7:4] + 1'h1;
+						end
+					end
+					else begin
+						m <= 8'h0;
+						if(h == 8'h11)
+							p = ~p;
+                        if(h < 8'h12) begin
+							if(h[3:0] < 4'h9)
+								h[3:0] <= h[3:0] + 1'h1;
+							else begin
+								h[3:0] <= 4'h0;
+								h[7:4] <= h[7:4] + 1'h1;
+							end	
+						end
+						else begin
+							h <= 1'h1;
+						end
+					end
+				end
+			end
+		end
+	end	
+	
+    assign pm = p;
+    assign hh = h;
+    assign mm = m;
+    assign ss = s;
+    
 endmodule

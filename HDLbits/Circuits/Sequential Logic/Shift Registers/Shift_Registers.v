e71626d09c944a3ff8504e525350b2418fe8f2d6
YZchen3467 <r07945002@g.ntu.edu.tw>
2022-06-24 07:36:05 +0800

    modify Shift_Registers.v

--- HDLbits/Circuits/Sequential Logic/Shift Registers/Shift_Registers.v
+++ HDLbits/Circuits/Sequential Logic/Shift Registers/Shift_Registers.v
@@ -42,3 +42,27 @@ module top_module(
 	end
 	
 endmodule
+
+////////////////////////////////////
+
+//Left/right arithmetic shift by 1 or 8
+module top_module(
+    input clk,
+    input load,
+    input ena,
+    input [1:0] amount,
+    input [63:0] data,
+    output reg [63:0] q); 
+	
+	always@(posedge clk) begin
+		if(load)
+			q <= data;
+		else
+			case(ena)
+				2'b00 : q <= {q[55:0], 8'b0};
+				2'b01 : q <= {q[0], q[99:1]};
+				2'b10 : q <= {q[98:0], q[99]};
+				default : q <= q;
+			endcase
+	
+endmodule

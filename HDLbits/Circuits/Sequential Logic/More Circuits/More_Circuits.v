ed304b2d4a3acd62cc5bb4acdc5f2fc9e7aedf37
YZchen3467 <r07945002@g.ntu.edu.tw>
2022-07-18 16:37:17 +0800

    modify rule 110

--- HDLbits/Circuits/Sequential Logic/More Circuits/More_Circuits.v
+++ HDLbits/Circuits/Sequential Logic/More Circuits/More_Circuits.v
@@ -36,7 +36,11 @@ module top_module(
 			q <= data;
 		end	
 		else begin
-			q[]
+			q[0] <= q[0];
+			q[511] <= q[511]^q[510] || q[510];
+			for(int i=1; i<511; i=i+1) begin
+				q[i] <= (q[i] ^ q[i-1]) | (!q[i+1] & q[i]);
+			end
 		end	
 	end
 	
@@ -63,6 +67,22 @@ module top_module(
 			for(j=0; j<16; j++) begin
 				if(i==0 && j==0)//top left 
 					sum = q_2d[15][1]+q_2d[15][0]+q_2d[15][15]+q_2d[0][1]+q_2d[0][15]+q_2d[1][0]+q_2d[1][1]+q_2d[1][15];
+				else if(i==0 && j==0)//top right
+					sum = sum=q_2d[0][0]+q_2d[0][14]+q_2d[15][0]+q_2d[15][14]+q_2d[15][15]+q_2d[1][0]+q_2d[1][14]+q_2d[1][15];
+				else if(i==15 && j==0)//bottom left
+                    sum = q_2d[15][1]+q_2d[15][15]+q_2d[14][0]+q_2d[14][15]+q_2d[14][1]+q_2d[0][0]+q_2d[0][1]+q_2d[0][15];
+                else if(i==15 && j==15)//bottom right
+                    sum = q_2d[15][0]+q_2d[15][14]+q_2d[14][15]+q_2d[14][0]+q_2d[14][14]+q_2d[0][0]+q_2d[0][15]+q_2d[0][14];
+				else if(i==0)//top border
+                    sum=q_2d[0][j-1]+q_2d[0][j+1]+q_2d[1][j-1]+q_2d[1][j]+q_2d[1][j+1]+q_2d[15][j-1]+q_2d[15][j]+q_2d[15][j+1];
+                else if(i==15)//bottom border
+                    sum=q_2d[15][j-1]+q_2d[15][j+1]+q_2d[0][j-1]+q_2d[0][j]+q_2d[0][j+1]+q_2d[14][j-1]+q_2d[14][j]+q_2d[14][j+1];
+                else if(j==0)//left border
+                    sum=q_2d[i][1]+q_2d[i][15]+q_2d[i-1][0]+q_2d[i-1][15]+q_2d[i-1][1]+q_2d[i+1][0]+q_2d[i+1][1]+q_2d[i+1][15];
+                else if(j==15)//right border
+                    sum=q_2d[i][0]+q_2d[i][14]+q_2d[i-1][0]+q_2d[i-1][14]+q_2d[i-1][15]+q_2d[i+1][0]+q_2d[i+1][14]+q_2d[i+1][15];
+                else  //mid item
+                    sum=q_2d[i-1][j]+q_2d[i-1][j-1]+q_2d[i-1][j+1]+q_2d[i][j-1]+q_2d[i][j+1]+q_2d[i+1][j]+q_2d[i+1][j-1]+q_2d[i+1][j+1];
 			end
 		end
 	end

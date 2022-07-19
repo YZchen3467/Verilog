987374be6730ef36b6ec9da578d14fd77b87890d
YZchen3467 <r07945002@g.ntu.edu.tw>
2022-07-19 17:19:16 +0800

    add more FSM problem

--- HDLbits/Circuits/Sequential Logic/Finite State Machine/Finte_State_Machine.v
+++ HDLbits/Circuits/Sequential Logic/Finite State Machine/Finte_State_Machine.v
@@ -274,3 +274,34 @@ endmodule
 ///////////////////////////////////////
 
 //Simple one-hot state transitions 3
+/*One-hot means one time one bit change*/
+module top_module(
+    input in,
+    input [3:0] state,
+    output [3:0] next_state,
+    output out); //
+
+    parameter A=0, B=1, C=2, D=3;
+
+    // State transition logic: Derive an equation for each state flip-flop.
+    assign next_state[A] = state[A]&~in | state[C]|~in;
+    assign next_state[B] = state[A]&in | state[B]&in | state[D]&in;
+    assign next_state[C] = state[B]&~in|state[D]&~in;
+    assign next_state[D] = state[C]&in;
+
+    // Output logic: 
+    assign out = state[D];
+
+endmodule
+
+///////////////////////////////////
+
+//Simple FSM 3(asynchronous)
+module top_module(
+	input clk,
+	input in,
+	input areset,
+	output out
+);
+	
+endmodule
